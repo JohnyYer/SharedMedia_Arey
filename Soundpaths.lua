f59e76d8856716aeece8b37ed6282f44fa1421e8
Ivan <johnyeremenko91@gmail.com>
2025-02-16 17:10:50 +0100

    initial commit

---
+++ Soundpaths.lua
@@ -0,0 +1,19 @@
+local LSM = LibStub("LibSharedMedia-3.0") 
+
+-- ----- 
+-- BACKGROUND 
+-- ----- 
+
+-- ----- 
+--  BORDER 
+-- ---- 
+
+-- -----
+--   FONT
+-- -----
+
+-- -----
+--   SOUND
+-- -----
+LSM:Register("sound", "|cFF800080logins_speachless|r", [[Interface\AddOns\SharedMedia_Arey\sound\logins_speachless.ogg]])
+LSM:Register("sound", "|cFF800080ovinax_raynor_feat_saifer|r", [[Interface\AddOns\SharedMedia_Arey\sound\ovinax_raynor_feat_saifer.ogg]])
